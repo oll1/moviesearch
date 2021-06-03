7bdbc626e07966720546d145d6861a928712a07d
oll1 <olli@olli.la>
2021-06-03 16:10:26 +0200

    fetch

--- movie.js
+++ movie.js
@@ -1,21 +1,22 @@
 var button = document.querySelector("input#search");
-button.addEventListener("click", search);
+button.addEventListener("click", loadApi);
 
 var input = document.querySelector("input#movie");
 
 
-function search()
+function loadApi()
 {
-	var request = new XMLHttpRequest();
-	request.addEventListener("load", log);
-	request.open("GET", "http://www.omdbapi.com/?apikey=167eb644&t="+input.value);
-	request.send();
+	fetch("http://www.omdbapi.com/?apikey=167eb644&t="+input.value)
+	.then(response => response.json())
+	.then(data => showResult(data));
 }
 
-function log()
+function showResult(data)
 {
-	var data = JSON.parse(this.responseText);
-	console.log(this.responseText);
+	console.log(data);
 	var title = document.querySelector("h1#title").innerHTML = data.Title;
 	var year = document.querySelector("h2#year").innerHTML = data.Year;
+	var runtime = document.querySelector("h3#runtime").innerHTML = data.Runtime;
+	var genre = document.querySelector("h4#genre").innerHTML = data.Genre;
 }
+
