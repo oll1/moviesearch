35e30524b70ce212873083baf7d82268c90a1b4e
oll1 <olli@olli.la>
2021-06-05 20:33:32 +0200

    refactor

--- movie.js
+++ movie.js
@@ -1,9 +1,3 @@
-find("button#search").addEventListener("click", loadApi);
-
-var input = find("input#title");
-var div = find("div#result");
-var ul = find("ul#ratings");
-
 function create(element)
 {
 	return document.createElement(element);
@@ -16,35 +10,54 @@ function find(element)
 {
 	return document.querySelector(element);
 }
+function findAll(elements)
+{
+	return document.querySelectorAll(elements);
+}
 function entry(id, content)
 {
-	var span = create("span");
+	let span = create("span");
 	span.innerHTML = content;
 	span.setAttribute('id', id);
 	append(div, span);
 }
-
-function loadApi()
-{
-	fetch("https://www.omdbapi.com/?apikey=167eb644&t="+input.value)
-	.then(response => response.json())
-	.then(function(data) {
-		console.log(data);
-		entry("title",`${data.Title} (${data.Year})`);
-		entry("genre",`Genre: ${data.Genre}`);
-
-		var ratings = data.Ratings;
-		ratings.map(function(rating)
-		{
-			var li = create("li");
-			var span = create("span");
-			span.innerHTML = `${rating.Source}: ${rating.Value}`;
-			append(li, span);
-			append(ul, li);
-		})
+find("button#search").addEventListener("click", getMovie);
+findAll("input").forEach(input => {
+	input.addEventListener("keypress", function(e) {
+		e.key == "Enter" && getMovie();
 	})
-	.catch(function(error) {
-		console.log(error);
-	});
-}
+});
+var movie = find("input#title");
+var year = find("input#year");
+var div = find("div#result");
+var ul = find("ul#ratings");
 
+function getMovie()
+{
+	if(!movie.value)
+		alert("no movie name given");
+	else
+	{
+		let url = `https://www.omdbapi.com/?apikey=167eb644&t=${movie.value}&y=${!isNaN(parseInt(year.value)) ? year.value : ""}`;
+		fetch(url)
+		.then(response => response.json())
+		.then(function(data) {
+			console.log(data);
+			entry("title",`${data.Title} (${data.Year})`);
+			entry("genre",`Genre: ${data.Genre}`);
+
+			let ratings = data.Ratings;
+			ratings.map(function(rating)
+			{
+				let li = create("li");
+				let span = create("span");
+				span.innerHTML = `${rating.Source}: ${rating.Value}`;
+				append(li, span);
+				append(ul, li);
+			})
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+	}
+}
